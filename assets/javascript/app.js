17e09ced9f32f2cfb0a64e5548db32bb79115abc
Machoman <rbunnicelli2010@yahoo.com>
2018-07-15 18:18:08 -0400

    javascript/

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var questions = [{
+        question: "Which NBA team won the most championships in the 2000's?";
+        answers: ["San Antonio Spurs", "Los Angeles Lakers", "Boston Celtics", "Miami Heat"];
+        correct: "Los Angeles Lakers";
+    }]
+}
+
+
+
